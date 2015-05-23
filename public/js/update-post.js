1d6760b1f20a5c24d6be3201ad73e10579247077
Joel Watson <joeljameswatson@gmail.com>
2015-05-23 16:35:15 -0700

    Added css js and wav sample

---
+++ public/js/update-post.js
@@ -0,0 +1,14 @@
+var post = { 2: 'What the f**k is goin on in this podcast??', 9: 'Not really..', 18: "That's cruelty to animals.", 25: 'Yum, badger burgers!!', 28: 'Noo. Save the badgers!' };
+
+var message = function(time){
+  if (post[time] != undefined){
+    document.getElementById('post').innerHTML = post[time];
+  }
+}
+
+var wave_time = function(){
+  return Math.floor(wavesurfer.getCurrentTime())
+}
+
+setInterval(function () {message(wave_time())}, 500);
+
