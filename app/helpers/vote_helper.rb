427ba1748ae1bc0d3310576d494a81bb72d3d244
awarewolfyukon <simon.pregent@hec.ca>
2015-05-23 01:55:38 +0000

    added Vote controller

---
+++ app/helpers/vote_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Intrjekt
+  class App
+    module VoteHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers VoteHelper
+  end
+end
