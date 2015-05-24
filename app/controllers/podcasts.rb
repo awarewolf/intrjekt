e5f36b4ca2cd7350405439195562d9d4dfdc15f1
Joel Watson <joeljameswatson@gmail.com>
2015-05-23 17:57:06 -0700

    Got podcasts

--- app/controllers/podcasts.rb
+++ app/controllers/podcasts.rb
@@ -20,31 +20,47 @@ Intrjekt::App.controllers :podcasts do
   # end
   
   get :index do
-
+    @podcasts = Podcast.all #Maybe add pagination?
+    render :index
   end
 
   get :new do
-
+    @podcast = Podcast.new
+    render :new
   end
 
-  post :create do
-
+  post :create, map: "podcasts" do
+    @podcast = Podcast.new(params[:podcast])
+    if @podcast.save
+      redirect "podcasts/#{@podcast.id}"
+    else
+      render :new
+    end
   end
 
-  get :show do
-
+  get :show, map: "podcasts/:id" do
+    @podcast = Podcast.find(params[:id])
+    render :show
   end
 
-  get :edit do
-
+  get :edit, map: "podcasts/:id/edit" do
+    @podcast = Podcast.find(params[:id])
+    render :edit
   end
 
-  put :update do
-
+  put :update, map: "podcasts/:id" do
+    @podcast = Podcast.find(params[:id])
+    if @podcast.update_attributes(params[:podcast])
+      redirect "podcasts/#{@podcast.id}"
+    else
+      render :edit
+    end
   end
 
-  delete :destroy do
-
+  delete :destroy, map: "podcasts/:id" do
+    @podcast = Podcast.find(params[:id])
+    @podcast.destroy
+    redirect "podcasts"
   end
 
 end
