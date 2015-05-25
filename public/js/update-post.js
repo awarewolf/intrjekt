29c33f01e627d8a7b8e6b204790f4fbfe1fa7974
no-tech <wjakesta@gmail.com>
2015-05-24 17:32:13 -0700

    uncommented update_post.js

--- public/js/update-post.js
+++ public/js/update-post.js
@@ -4,24 +4,24 @@ $(function() {
   var fileLocation = '/audio/CarbonFootprint.wav'
   wavesurfer.load(fileLocation)
 
-//   // post = { 2: 'What the f**k is goin on in this podcast??', 9: 'Not really..', 18: "That's cruelty to animals.", 25: 'Yum, badger burgers!!', 28: 'Noo. Save the badgers!' };
+  // post = { 2: 'What the f**k is goin on in this podcast??', 9: 'Not really..', 18: "That's cruelty to animals.", 25: 'Yum, badger burgers!!', 28: 'Noo. Save the badgers!' };
   
-//   $.getJSON( "comments.json", function( data ) {
-//     console.log("get JSON")
-//     post = data
-// })
-//   .fail(function() {
-//     console.log( "error" );
-//  });
+  $.getJSON( "comments.json", function( data ) {
+    console.log("get JSON")
+    post = data
+})
+  .fail(function() {
+    console.log( "error" );
+ });
 
-//   var message = function(time){
-//     if (post[time] != undefined){
-//       document.getElementById('post').innerHTML = post[time];
-//     }
-//   }
-//   var wave_time = function(){
-//     return Math.floor(wavesurfer.getCurrentTime())
-//   }
-//   setInterval(function () {message(wave_time())}, 500);
+  var message = function(time){
+    if (post[time] != undefined){
+      document.getElementById('post').innerHTML = post[time];
+    }
+  }
+  var wave_time = function(){
+    return Math.floor(wavesurfer.getCurrentTime())
+  }
+  setInterval(function () {message(wave_time())}, 500);
 
 });
